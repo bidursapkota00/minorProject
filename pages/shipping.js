c346bcfe2488d0b2dbe99559decac97990c69abd
bidursapkota00 <bidursapkota00@gmail.com>
2021-11-13 20:13:33 +0545

    connected to mongodb atlas

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,8 +1,9 @@
 import router from 'next/router';
 import React, { useContext } from 'react';
 import { Store } from '../util/Store';
+import dynamic from 'next/dynamic';
 
-export default function Shipping() {
+function Shipping() {
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
   if (!userInfo) {
@@ -10,3 +11,5 @@ export default function Shipping() {
   }
   return <div>Shipping</div>;
 }
+
+export default dynamic(() => Promise.resolve(Shipping), { ssr: false });
